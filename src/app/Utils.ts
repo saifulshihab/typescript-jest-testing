2defce39d7b91ff06bc732b15ae4bbc5397921b6
saifulshihab <shihabmd1970@gmail.com>
2022-02-08 17:03:46 +0600

    config file update & test added

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,13 +1,15 @@
 import { UrlWithParsedQuery, parse } from 'url';
 
+/* istanbul ignore next */
 export class Utils {
   public static toUpperCase(arg: string): string {
     return arg.toUpperCase();
   }
 
-  // parsred url
-
   public static parseURL(url: string): UrlWithParsedQuery {
+    if (!url) {
+      throw new Error('Invalid URL!');
+    }
     return parse(url, true);
   }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,14 @@
 import { Utils } from '../app/Utils';
 
 describe('Utils file test', () => {
+  // beforeAll(() => {
+  //   console.log('before all');
+  // });
+
+  // beforeEach(() => {
+  //   console.log('before each');
+  // });
+
   test('text uppercase', () => {
     const upperStr = Utils.toUpperCase('shihab');
     expect(upperStr).toBe('SHIHAB');
@@ -30,5 +38,27 @@ describe('Utils file test', () => {
       pass: '1234',
     };
     expect(parsedURL.query).toEqual(expectedQuery);
+  });
+
+  test('test invalid URL', () => {
+    function expectError() {
+      Utils.parseURL('');
+    }
+    expect(expectError).toThrowError();
+  });
+
+  test('test invalid URL with arrow function', () => {
+    expect(() => {
+      Utils.parseURL('');
+    }).toThrowError();
+  });
+
+  test.only('test invalid URL error with try catch', () => {
+    try {
+      Utils.parseURL('');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty('message', 'Invalid URL!');
+    }
   });
 });
